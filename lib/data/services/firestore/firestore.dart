50db92a1ebcb928eba1295b4a42ae8baf7265781
Sobachynska <kn1b21.sobachynska@kpnu.edu.ua>
2025-01-29 22:40:00 +0200

    29 01

--- lib/data/services/firestore/firestore.dart
+++ lib/data/services/firestore/firestore.dart
@@ -1,35 +1,35 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-
-// class FirestoreService {
-//   final CollectionReference marks =
-//       FirebaseFirestore.instance.collection('marks');
-//   //CRUD
-//   Stream<QuerySnapshot> getMarks() {
-//     final marksStream = marks.orderBy('value', descending: true).snapshots();
-//     return marksStream;
-//   }
-// }
-
-// services/firestore_service.dart
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreService {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  // Отримати оцінки для конкретної групи
-  Future<List<Map<String, dynamic>>> fetchGrades(String groupId) async {
-    try {
-      final snapshot = await _firestore
-          .collection('groups')
-          .doc(groupId)
-          .collection('grades')
-          .get();
-
-      return snapshot.docs.map((doc) => doc.data()).toList();
-    } catch (e) {
-      throw Exception('Помилка при отриманні даних: $e');
-    }
+  final CollectionReference marks =
+      FirebaseFirestore.instance.collection('marks');
+  //CRUD
+  Stream<QuerySnapshot> getMarks() {
+    final marksStream = marks.orderBy('value', descending: true).snapshots();
+    return marksStream;
   }
-
-  fetchStudentData(String groupId, String studentEmail) {}
 }
+
+// services/firestore_service.dart
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+// class FirestoreService {
+//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+//   // Отримати оцінки для конкретної групи
+//   Future<List<Map<String, dynamic>>> fetchGrades(String groupId) async {
+//     try {
+//       final snapshot = await _firestore
+//           .collection('groups')
+//           .doc(groupId)
+//           .collection('grades')
+//           .get();
+
+//       return snapshot.docs.map((doc) => doc.data()).toList();
+//     } catch (e) {
+//       throw Exception('Помилка при отриманні даних: $e');
+//     }
+//   }
+
+//   fetchStudentData(String groupId, String studentEmail) {}
+// }
